6f02420f49e68d5d5b3893dc73881aeea045d41f
Fitria Inta <fitriainta07@gmail.com>
2026-04-22 15:53:03 +0700

    before

--- tugas/counter_pengunjung.php
+++ tugas/counter_pengunjung.php
@@ -13,6 +13,8 @@ class Pengunjung {
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung(); 
 
 echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
 
